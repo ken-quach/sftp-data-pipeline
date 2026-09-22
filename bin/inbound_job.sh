6e69b6c791e10f242c2aa72b6cf90bc7869c6295
Kenneth Quach <kennethquach@Kenneths-MacBook-Pro.local>
2026-09-22 17:02:38 -0400

    Add inbound file validation and logging

--- bin/inbound_job.sh
+++ bin/inbound_job.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-echo "Starting inbound SFTP job..."
-echo "Current Directory: $(pwd)"
-echo "Job completed."
+JOB_NAME="inbound_job"
+BASE_DIR="/Users/kennethquach/projects/sftp-data-pipeline"
+INBOUND_DIR="$BASE_DIR/ftp/inbound"
+LOG_DIR="$BASE_DIR/log"
+LOG_FILE="$LOG_DIR/inbound_job.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+FILE_NAME="vendor_customer.csv"
+
+echo "Starting $JOB_NAME..."
+echo "Project Directory: $BASE_DIR"
+echo "Inbound Directory: $INBOUND_DIR"
+
+if [ -f "$INBOUND_DIR/$FILE_NAME" ]; then
+	echo "File found: $FILE_NAME"
+	echo "[$TIMESTAMP] SUCCESS: File found: $FILE_NAME" >> "$LOG_FILE"
+else
+	echo "ERROR: File not found: $FILE_NAME"
+	echo "[$TIMESTAMP] ERROR: File not found: $FILE_NAME" >> "$LOG_FILE"
+	exit 1
+fi
+echo "Job completed successfully."
+echo "[$TIMESTAMP] SUCCESS: $JOB_NAME completed successfully." >> "$LOG_FILE"
+exit 0
